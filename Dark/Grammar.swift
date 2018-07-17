fe4d259cfddc0cdecdc082ec2453932ac16fd7f7
Andrea Tomarelli <andrea.tomarelli@gmail.com>
2018-07-17 22:46:14 +0200

    Dark stuff

--- Dark/Grammar.swift
+++ Dark/Grammar.swift
@@ -8,27 +8,23 @@
 
 enum Grammar {
 
-    typealias CHAR_UNDERSCORE = Void
-    
-    
-    typealias PUNC_EQUAL = Void
-    typealias PUNC_DOUBLE_QUOTE = Void
+    typealias PUNC_LBRACE = Void
+    typealias PUNC_RBRACE = Void
+    typealias PUNC_LPAREN = Void
+    typealias PUNC_RPAREN = Void
+
+    typealias PUNC_COMMA = Void
+    typealias PUNC_COLON = Void
+
+    typealias PUNC_ARROW = Void
 
     
-    enum AssignmentExpression {
-        
-    }
+    typealias KW_FUNC = Void
+
     
-    enum DecimalDigit: String {
-        
-        case _0 = "Numbers 0 ... 9"
-        
-    }
+    enum ArgumentName {
     
-    enum DecimalLiteralCharacter {
-        
-        case _0(DecimalDigit)
-        case _1(CHAR_UNDERSCORE)
+        case _0(Identifier)
         
     }
     
@@ -38,428 +34,62 @@ enum Grammar {
         
     }
 
-    enum Expression {
-        
-        case _0(AssignmentExpression)
-        case _1(FunctionCallExpression)
-        case _2(PrimaryExpression)
+    enum FunctionArgument {
+
+        case _0(ArgumentName, PUNC_COLON, Type)
 
     }
-
-    enum FunctionCallExpression {
+    
+    indirect enum FunctionArgumentList {
+        
+        case _0(FunctionArgument)
+        case _1(FunctionArgument, PUNC_COMMA, FunctionArgumentList)
+        
+    }
+    
+    enum FunctionArguments {
+        
+        case _0(PUNC_LPAREN, PUNC_RPAREN)
+        case _1(PUNC_LPAREN, FunctionArgumentList, PUNC_RPAREN)
+        
+    }
+    
+    enum FunctionBody {
+    
+        case _0(PUNC_LBRACE, PUNC_RBRACE)
         
     }
     
     enum FunctionDeclaration {
+    
+        case _0(KW_FUNC, FunctionName, FunctionSignature, FunctionBody)
         
     }
     
-    enum Identifier {
+    enum FunctionName {
         
-        case _0(IdentifierHead, [IdentifierCharacter])
+        case _0(Identifier)
+        
+    }
+   
+    enum FunctionSignature {
+        
+        case _0(FunctionArguments, PUNC_ARROW, Type)
         
     }
     
-    enum IdentifierCharacter: String {
-        
-        case _0 = "a ... z"
-        case _1 = "A ... Z"
-        case _2 = "0 ... 9"
-        case _3 = "_"
-        
-    }
-    
-    enum IdentifierHead: String {
-        
-        case _0 = "a ... z"
-        case _1 = "A ... Z"
-        case _3 = "_"
-        
-    }
-    
-    enum IntegerLiteral {
-        
-        case _0(DecimalDigit, [DecimalLiteralCharacter])
-        
-    }
-    
-    enum Literal {
-        
-        case _0(StringLiteral)
-        case _1(IntegerLiteral)
-
-    }
-    
-    enum PrimaryExpression {
-        
-        case _0(Literal)
-        
-    }
-    
-    enum StringLiteral {
-        
-        case _0(PUNC_DOUBLE_QUOTE, [StringLiteralCharacter], PUNC_DOUBLE_QUOTE)
-        
-    }
-    
-    enum StringLiteralCharacter: String {
-
-        case _0 = "Almost any character (no new line, no unescaped characters, ...)"
-        case _1 = "\\"
-        case _2 = "\""
-
-    }
+    enum Identifier { }
     
     enum TopLevelExpression {
         
-        case _0(Declaration)
-        case _1(Expression)
+        case _0([Declaration])
 
     }
     
+    enum `Type` {
+        
+        case _0(Identifier)
+        
+    }
     
-    
-//
-//
-//
-//
-//
-//    typealias KW_FUNC = Void
-//    typealias KW_STRUCT = Void
-//    typealias KW_LET = Void
-//
-//
-//    typealias PUNC_LPAREN = Void
-//    typealias PUNC_RPAREN = Void
-//    typealias PUNC_LBRACE = Void
-//    typealias PUNC_RBRACE = Void
-//
-//    typealias PUNC_COMMA = Void
-//    typealias PUNC_COLON = Void
-//
-//    typealias PUNC_ARROW = Void
-//
-//
-//    enum CodeBlock {
-//
-//        case _0(PUNC_LBRACE, [Statement], PUNC_RBRACE)
-//
-//    }
-//
-//    enum ConstantDeclaration {
-//
-//        case _0(KW_LET, Identifier, TypeAnnotation?)
-//
-//    }
-//
-//    enum Declaration {
-//
-//        case _0(FunctionDeclaration)
-//        case _1(StructDeclaration)
-//        case _2(ConstantDeclaration)
-//
-//    }
-//
-//    enum FunctionBody {
-//
-//        case _0(CodeBlock)
-//
-//    }
-//
-//    enum FunctionDeclaration {
-//
-//        case _0(KW_FUNC, FunctionName, FunctionSignature, FunctionBody)
-//
-//    }
-//
-//    enum FunctionName {
-//
-//        case _0(Identifier)
-//
-//    }
-//
-//    enum FunctionResult {
-//
-//        case _0(PUNC_ARROW, Type)
-//
-//    }
-//
-//    enum FunctionSignature {
-//
-//        case _0(ParameterClause, FunctionResult?)
-//
-//    }
-//
-//    enum Identifier {
-//
-//        case _0(IdentifierHead, [IdentifierCharacter])
-//
-//    }
-//
-//    enum IdentifierCharacter: String {
-//
-//        case _0 = "a ... z"
-//        case _1 = "A ... Z"
-//        case _2 = "0 ... 9"
-//        case _3 = "_"
-//
-//    }
-//
-//    enum IdentifierHead: String {
-//
-//        case _0 = "a ... z"
-//        case _1 = "A ... Z"
-//        case _3 = "_"
-//
-//    }
-//
-//    enum Parameter {
-//
-//        case _0(ParameterName, TypeAnnotation)
-//
-//    }
-//
-//    enum ParameterClause {
-//
-//        case _0(PUNC_LPAREN, ParameterList?, PUNC_RPAREN)
-//
-//    }
-//
-//    indirect enum ParameterList {
-//
-//        case _0(Parameter)
-//        case _1(Parameter, PUNC_COMMA, ParameterList)
-//
-//    }
-//
-//    enum ParameterName {
-//
-//        case _0(Identifier)
-//
-//    }
-//
-//    enum Statement {
-//
-//        case _0(Declaration)
-//
-//    }
-//
-//    enum StructBody {
-//
-//        case _0(PUNC_LBRACE, [StructMember], PUNC_RBRACE)
-//
-//    }
-//
-//    enum StructDeclaration {
-//
-//        case _0(KW_STRUCT, StructName, StructBody)
-//
-//    }
-//
-//    enum StructMember {
-//
-//        case _0(Declaration)
-//
-//    }
-//
-//    enum StructName {
-//
-//        case _0(Identifier)
-//
-//    }
-//
-//    enum TopLevelDeclaration {
-//
-//        case _0([Statement])
-//
-//    }
-//
-//    enum `Type` {
-//
-//        case _0(TypeIdentifier)
-//
-//    }
-//
-//    enum TypeAnnotation {
-//
-//        case _0(PUNC_COLON, Type)
-//
-//    }
-//
-//    enum TypeIdentifier {
-//
-//        case _0(Identifier)
-//
-//    }
-    
-    
-    
-    
-    
-    //postfix operator *
-    //
-    //postfix func *(rhs: Any) { }
-    //
-    //func Grammar() {
-    //    var code_block: Any = "code_block"
-    //
-    //    var declaration: Any = "declaration"
-    //
-    //    var expression: Any = "expression"
-    //    var function_call_expression: Any = "function_call_expression"
-    //    var function_call_argument: Any = "function_call_argument"
-    //    var function_call_argument_list: Any = "function_call_argument_list"
-    //    var function_call_arguments_clause: Any = "function_call_arguments_clause"
-    //
-    //    var function_body: Any = "function_body"
-    //    var function_declaration: Any = "function_declaration"
-    //    var function_name: Any = "function_name"
-    //    var function_result: Any = "function_result"
-    //    var function_signature: Any = "function_signature"
-    //
-    //    var identifier: Any = "identifier"
-    //    var identifier_character: Any = "identifier_character"
-    //    var identifier_head: Any = "identifier_head"
-    //
-    //    var parameter: Any = "parameter"
-    //    var parameter_clause: Any = "parameter_clause"
-    //    var parameter_list: Any = "parameter_list"
-    //    var parameter_name: Any = "parameter_name"
-    //
-    //    var statement: Any = "statement"
-    //
-    //    var struct_body: Any = "struct_body"
-    //    var struct_declaration: Any = "struct_declaration"
-    //    var struct_member: Any = "struct_member"
-    //    var struct_name: Any = "struct_name"
-    //
-    //    var top_level_declaration: Any = "top_level_declaration"
-    //
-    //    var type: Any = "type"
-    //    var type_annotation: Any = "type_annotation"
-    //
-    //
-    //    code_block = [
-    //        ["{", statement*, "}"]
-    //    ]
-    //
-    //    declaration = [
-    //        function_declaration,
-    //        struct_declaration
-    //    ]
-    //
-    //    expression = [
-    //        function_call_expression
-    //    ]
-    //
-    //    function_body = [
-    //        code_block
-    //    ]
-    //
-    //    function_call_argument = [
-    //        expression,
-    //        [identifier, ":", expression]
-    //    ]
-    //
-    //    function_call_argument_list = [
-    //        function_call_argument,
-    //        [function_call_argument, ",", function_call_argument_list]
-    //    ]
-    //
-    //    function_call_arguments_clause = [
-    //        ["(", ")"],
-    //        ["(", function_call_argument_list, ")"]
-    //    ]
-    //
-    //    function_call_expression = [
-    //        [expression, function_call_arguments_clause]
-    //    ]
-    //
-    //    function_declaration = [
-    //        ["func", function_name, function_signature, function_body]
-    //    ]
-    //
-    //    function_name = [
-    //        identifier
-    //    ]
-    //
-    //    function_result = [
-    //        ["->", type]
-    //    ]
-    //
-    //    function_signature = [
-    //        [parameter_clause, function_result]
-    //    ]
-    //
-    //    identifier = [
-    //        [identifier_head, identifier_character*]
-    //    ]
-    //
-    //    identifier_character = [
-    //        "[a-z]",
-    //        "[A-Z]",
-    //        "[0-9]",
-    //        "_"
-    //    ]
-    //
-    //    identifier_head = [
-    //        "[a-z]",
-    //        "[A-Z]",
-    //        "_"
-    //    ]
-    //
-    //    parameter = [
-    //        [parameter_name, type_annotation]
-    //    ]
-    //
-    //    parameter_clause = [
-    //        ["(", ")"],
-    //        ["(", parameter_list, ")"]
-    //    ]
-    //
-    //    parameter_name = [
-    //        identifier
-    //    ]
-    //
-    //    parameter_list = [
-    //        parameter,
-    //        [parameter, ",", parameter_list]
-    //    ]
-    //
-    //    statement = [
-    //        declaration,
-    //        expression
-    //    ]
-    //
-    //    struct_body = [
-    //        ["{", struct_member*, "}"]
-    //    ]
-    //
-    //    struct_declaration = [
-    //        ["struct", struct_name, struct_body]
-    //    ]
-    //
-    //    struct_member = [
-    //        declaration
-    //    ]
-    //
-    //    struct_name = [
-    //        identifier
-    //    ]
-    //
-    //    top_level_declaration = [
-    //        statement*
-    //    ]
-    //
-    //    type = [
-    //        identifier
-    //    ]
-    //
-    //    type_annotation = [
-    //        [":", type]
-    //    ]
-    //
-    //    print(top_level_declaration)
-    //}
 }
-
--- Dark/Parser.swift
+++ Dark/Parser.swift
@@ -6,24 +6,219 @@
 //  Copyright © 2018 Weedea. All rights reserved.
 //
 
-struct ParserError: Error {
+protocol DarkToken {
     
-    let message: String
-    
+    func `is`(_ other: DarkToken) -> Bool
+  
 }
 
-struct Parser<Buffer: BidirectionalCollection> where Buffer.Element == Character {
- 
-    var lexer: Lexer<Buffer>
+extension DarkToken {
     
-    init(lexer: Lexer<Buffer>) {
-        self.lexer = lexer
+    func `is`<T: DarkToken>(_ other: T.Type) -> Bool {
+        return self is T
     }
     
 }
 
-extension Parser {
+enum DarkKeyword: DarkToken {
     
+    case `func`
+    
+    
+    func `is`(_ other: DarkToken) -> Bool {
+        if let k = other as? DarkKeyword {
+            return self == k
+        } else {
+            return false
+        }
+    }
+    
+}
+
+struct DarkIdentifier: DarkToken {
+    
+    let id: String
+    
+    
+    func `is`(_ other: DarkToken) -> Bool {
+        if let k = other as? DarkIdentifier {
+            return self.id == k.id
+        } else {
+            return false
+        }
+    }
+    
+}
+
+enum DarkPunctuator: DarkToken {
+    
+    case lParen
+    case rParen
+    case lBrace
+    case rBrace
+    
+    case comma
+    case colon
+    
+    case arrow
+    
+    
+    func `is`(_ other: DarkToken) -> Bool {
+        if let p = other as? DarkPunctuator {
+            return self == p
+        } else {
+            return false
+        }
+    }
+
+}
+
+struct DarkLexer {
+    
+    let currentToken: DarkToken
+
+    func lex() {
+        
+    }
+    
+}
+
+struct DarkParser {
+
+    private let lexer: DarkLexer
+
+    
+    func parseArgumentName() -> Bool {
+        guard lexer.currentToken.is(DarkIdentifier.self) else { return false }
+        
+        lexer.lex()
+        
+        return true
+    }
+    
+    func parseDeclaration() -> Bool {
+        return parseFunctionDeclaration()
+    }
+
+    func parseFunctionArgumentList() -> Bool {
+        guard parseFunctionArgument() else { return false }
+        
+        guard lexer.currentToken.is(DarkPunctuator.comma) else { return true }
+    
+        lexer.lex()
+       
+        return parseFunctionArgumentList()
+    }
+    
+    func parseFunctionArgument() -> Bool {
+        guard parseArgumentName() else { return false }
+    
+        guard lexer.currentToken.is(DarkPunctuator.colon) else { return false }
+        
+        lexer.lex()
+        
+        return parseType()
+    }
+    
+    func parseFunctionArguments() -> Bool {
+        guard lexer.currentToken.is(DarkPunctuator.lParen) else { return false }
+        
+        lexer.lex()
+
+        _ = parseFunctionArgumentList()
+        
+        guard lexer.currentToken.is(DarkPunctuator.rParen) else { return false }
+
+        lexer.lex()
+        
+        return true
+    }
+    
+    func parseFunctionBody() -> Bool {
+        guard lexer.currentToken.is(DarkPunctuator.lBrace) else { return false }
+        
+        lexer.lex()
+        
+        guard lexer.currentToken.is(DarkPunctuator.rBrace) else { return false }
+        
+        lexer.lex()
+
+        return true
+    }
+    
+    func parseFunctionDeclaration() -> Bool {
+        guard lexer.currentToken.is(DarkKeyword.func) else { return false }
+        
+        lexer.lex()
+        
+        guard parseFunctionName() else { return false }
+
+        guard parseFunctionSignature() else { return false }
+        
+        return parseFunctionBody()
+    }
+    
+    func parseFunctionName() -> Bool {
+        guard lexer.currentToken.is(DarkIdentifier.self) else { return false }
+
+        lexer.lex()
+        
+        return true
+    }
+    
+    func parseFunctionSignature() -> Bool {
+        guard parseFunctionArguments() else { return false }
+        
+        guard lexer.currentToken.is(DarkPunctuator.arrow) else { return false }
+        
+        lexer.lex()
+     
+        return parseType()
+    }
+
+    func parseTopLevelExpression() -> Bool {
+        while parseDeclaration() {
+            // empty
+        }
+        
+        return true
+    }
+    
+    func parseType() -> Bool {
+        guard lexer.currentToken.is(DarkIdentifier.self) else { return false }
+
+        lexer.lex()
+        
+        return true
+    }
+    
+}
+
+
+
+
+
+
+
+
+struct ParserError: Error {
+
+    let message: String
+
+}
+
+struct Parser<Buffer: BidirectionalCollection> where Buffer.Element == Character {
+
+    var lexer: Lexer<Buffer>
+
+    init(lexer: Lexer<Buffer>) {
+        self.lexer = lexer
+    }
+
+}
+
+extension Parser {
+
     mutating func parseCodeBlock() throws {
         print("Parsing code block")
 
@@ -31,16 +226,16 @@ extension Parser {
             let lBrace = lexer.currentToken as? Punctuator,
             lBrace == .lBrace
             else { throw ParserError(message: "Expected '{'") }
-      
+
         lexer.lex()
-        
+
         if let rBrace = lexer.currentToken as? Punctuator, rBrace == .rBrace {
             print("Parsed empty code block")
             return lexer.lex()
         }
-        
+
         try parseStatement()
-        
+
         guard
             let rBrace = lexer.currentToken as? Punctuator,
             rBrace == .rBrace
@@ -48,19 +243,19 @@ extension Parser {
 
         print("Parsed code block")
     }
-    
+
     mutating func parseDeclaration() throws {
         print("Parsing declaration")
         try parseFunctionDeclaration()
         print("Parsed declaration")
     }
-    
+
     mutating func parseFunctionBody() throws {
         print("Parsing function body")
         try parseCodeBlock()
         print("Parsed function body")
     }
-    
+
     mutating func parseFunctionDeclaration() throws {
         print("Parsing function declaration")
 
@@ -68,37 +263,37 @@ extension Parser {
             let t = lexer.currentToken as? Keyword,
             t == .func
             else { throw ParserError(message: "Expected 'func' keyword") }
-        
+
         lexer.lex()
-        
+
         try parseFunctionName()
         try parseFunctionSignature()
         try parseFunctionBody()
-        
+
         print("Parsed function declaration")
     }
-    
+
     mutating func parseFunctionName() throws {
         print("Parsing function name")
         try parseIdentifier()
         print("Parsed function name")
     }
-    
+
     mutating func parseFunctionResult() throws {
         print("Parsing function result")
-        
+
         guard
             let arrow = lexer.currentToken as? Punctuator,
             arrow == .arrow
             else { throw ParserError(message: "Expected '->'") }
 
         lexer.lex()
-        
+
         try parseType()
-        
+
         print("Parsed function result")
     }
-    
+
     mutating func parseFunctionSignature() throws {
         print("Parsing function signature")
         try parseParametersClause()
@@ -112,19 +307,19 @@ extension Parser {
         guard let id = lexer.currentToken as? Identifier else {
             throw ParserError(message: "Expected identifier")
         }
-        
+
         lexer.lex()
-        
+
         print("Parsed identifier '\(id.identifier)'")
     }
-    
+
     mutating func parseParameter() throws {
         print("Parsing parameter")
         try parseParameterName()
         try parseTypeAnnotation()
         print("Parsed parameter")
     }
-    
+
     mutating func parseParameterName() throws {
         print("Parsing parameter name")
         try parseIdentifier()
@@ -138,23 +333,23 @@ extension Parser {
             let lParen = lexer.currentToken as? Punctuator,
             lParen == .lParen
             else { throw ParserError(message: "Expected '('") }
-        
+
         lexer.lex()
-        
+
         if let rParen = lexer.currentToken as? Punctuator, rParen == .rParen {
             print("Parsed empty parameters clause")
             return lexer.lex()
         }
-        
+
         try parseParametersList()
-    
+
         guard
             let rParen = lexer.currentToken as? Punctuator,
             rParen == .rParen
             else { throw ParserError(message: "Expected ')'") }
 
         lexer.lex()
-        
+
         print("Parsed parameters clause")
     }
 
@@ -162,14 +357,14 @@ extension Parser {
         print("Parsing parameters list")
 
         try parseParameter()
-        
+
         if let t = lexer.currentToken as? Punctuator, t == .comma {
             try parseParametersList()
         }
-        
+
         print("Parsed parameters list")
     }
-    
+
     mutating func parseStatement() throws {
         print("Parsing statement")
         try parseDeclaration()
@@ -187,7 +382,7 @@ extension Parser {
         try parseIdentifier()
         print("Parsed type")
     }
-    
+
     mutating func parseTypeAnnotation() throws {
         print("Parsing type annotation")
 
@@ -195,12 +390,12 @@ extension Parser {
             let colon = lexer.currentToken as? Punctuator,
             colon == .colon
             else { throw ParserError(message: "Expected ':'") }
-        
+
         lexer.lex()
-    
+
         try parseType()
-   
+
         print("Parsed type annotation")
     }
-    
+
 }
