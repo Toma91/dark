7bfaa529bcee0b9dd89c599ffff44f8843b7fb21
Andrea Tomarelli <andrea.tomarelli@gmail.com>
2018-07-18 23:59:17 +0200

    Restart

--- Dark/main.swift
+++ Dark/main.swift
@@ -14,37 +14,54 @@ func unimpl(function: String = #function) -> Never {
     fatalError("\(function): not implemented yet")
 }
 
-
-
-let _code = """
-func test(name: String) -> Void {
+func t<Buffer: RandomAccessCollection>(buf: Buffer) where Buffer.Element == Character {
+    
 }
-test(name: characterName)
-"""
 
-var dp = DarkParser(lexer: DarkLexer(buffer: _code))
-print(dp.parseTopLevelExpression())
+var s = ""
 
-exit(0)
-
-
-
-
-let code = """
-func test(name: String) -> Void {
-    print(name)
+for i in 0 ..< 1_000_000_000 {
+    s.append(Character(Unicode.Scalar(UInt8(i % 256))))
 }
-test(name: characterName)
-"""
-/*var _lexer = Lexer(buffer: code)
 
-while let t = Optional(_lexer.getToken()), !t.isEof {
-    print("*** got token:", t)
-}*/
+let d = Date()
+let b = Array.init(s)
+print(Date().timeIntervalSince(d))
 
-var l = Lexer(buffer: code)
-var p = Parser(lexer: l)
-try p.parseTopLevelExpression()
+t(buf: b)
+
+
+
+
+//let _code = """
+//func test(name: String) -> Void {
+//}
+//test(name: characterName)
+//"""
+//
+//var dp = DarkParser(lexer: DarkLexer(buffer: _code))
+//print(dp.parseTopLevelExpression())
+//
+//exit(0)
+//
+//
+//
+//
+//let code = """
+//func test(name: String) -> Void {
+//    print(name)
+//}
+//test(name: characterName)
+//"""
+///*var _lexer = Lexer(buffer: code)
+//
+//while let t = Optional(_lexer.getToken()), !t.isEof {
+//    print("*** got token:", t)
+//}*/
+//
+//var l = Lexer(buffer: code)
+//var p = Parser(lexer: l)
+//try p.parseTopLevelExpression()
 
 
 
